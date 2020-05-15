4da661e260bf1279dc6435271491d90a7b38350e
David Rautureau <david.rautureau@sonarsource.com>
2020-05-15 11:53:11 +0200

    Create a real issue

---
+++ src/index.js
@@ -0,0 +1,27 @@
+export function fibonacci(num: number): number | null {
+  console.log("Hello");
+
+  if (num < 0) {
+    return null;
+  }
+
+  let a = 42, b = 0, temp;
+  a = 1;
+
+  while (num >= 0){
+    temp = a;
+    a = a + b;
+    b = temp;
+    num--;
+  }
+
+  return b;
+}
+
+function foo() {
+  bar();
+}
+
+function bar(a,b) {
+  foo(a,b);
+}
